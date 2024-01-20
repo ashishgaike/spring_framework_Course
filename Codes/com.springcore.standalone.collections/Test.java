c54740c90c055922c05648cd5c9977ad24044137
Ashish Gaike <71187936+ashishgaike@users.noreply.github.com>
2024-01-20 23:07:14 +0530

    Create Test.java
    Main file

---
+++ Codes/com.springcore.standalone.collections/Test.java
@@ -0,0 +1,24 @@
+package com.springcore.standalone.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext con = new ClassPathXmlApplicationContext(
+				"com/springcore/standalone/collections/aloneconfig.xml");
+		Person person1 = con.getBean("person1", Person.class);
+		System.out.println(person1);
+		System.out.println(person1.getFriends().getClass().getName());
+		System.out.println("____________________________________________");
+		System.out.println(person1.getFeesstructure());
+		System.out.println(person1.getFeesstructure().getClass());
+		System.out.println("____________________________________________");
+		System.out.println(person1.getProperties());
+
+	}
+
+}
