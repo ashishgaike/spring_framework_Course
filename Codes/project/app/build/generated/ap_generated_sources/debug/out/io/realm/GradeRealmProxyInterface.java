6b18dc75e13aa1f7f9d79fa0e358562e2fb5e75b
ashishgaike <ashishgaike2001@gmail.com>
2024-02-15 14:16:00 +0530

    grade proxy

---
+++ Codes/project/app/build/generated/ap_generated_sources/debug/out/io/realm/GradeRealmProxyInterface.java
@@ -0,0 +1,21 @@
+package io.realm;
+
+
+public interface GradeRealmProxyInterface {
+    public Integer realmGet$id();
+    public void realmSet$id(Integer value);
+    public String realmGet$subject();
+    public void realmSet$subject(String value);
+    public String realmGet$class_score();
+    public void realmSet$class_score(String value);
+    public String realmGet$exam_score();
+    public void realmSet$exam_score(String value);
+    public String realmGet$total_score();
+    public void realmSet$total_score(String value);
+    public String realmGet$grade();
+    public void realmSet$grade(String value);
+    public String realmGet$term();
+    public void realmSet$term(String value);
+    public String realmGet$year();
+    public void realmSet$year(String value);
+}
