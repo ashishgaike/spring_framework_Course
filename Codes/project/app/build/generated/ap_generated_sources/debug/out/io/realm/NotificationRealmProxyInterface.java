652f192abc21b99ab98c22c073b5ea7d5e86b262
ashishgaike <ashishgaike2001@gmail.com>
2024-02-18 12:34:18 +0530

    interface

---
+++ Codes/project/app/build/generated/ap_generated_sources/debug/out/io/realm/NotificationRealmProxyInterface.java
@@ -0,0 +1,17 @@
+package io.realm;
+
+
+public interface NotificationRealmProxyInterface {
+    public Integer realmGet$id();
+    public void realmSet$id(Integer value);
+    public String realmGet$noticetype();
+    public void realmSet$noticetype(String value);
+    public String realmGet$description();
+    public void realmSet$description(String value);
+    public String realmGet$venue();
+    public void realmSet$venue(String value);
+    public String realmGet$createdDate();
+    public void realmSet$createdDate(String value);
+    public boolean realmGet$read();
+    public void realmSet$read(boolean value);
+}
